07faa3dddc0f907220abc46c1c834c86cb3dfe07
carlosdanielpohlod <carlospohlod@gmail.com>
2024-07-15 21:58:20 -0300

    refactor: improve code quality on rails_tracepoint_stack, creating a TracepointStack class

--- lib/rails_tracepoint_stack.rb
+++ lib/rails_tracepoint_stack.rb
@@ -1,17 +1,40 @@
-gem_paths = Bundler.load.specs.map(&:full_gem_path)
-ruby_lib_path = RbConfig::CONFIG['rubylibdir']
 
-trace = TracePoint.new(:call) do |tp|
-  next if tp.path.start_with?('<internal:') || tp.path == '(eval)' || gem_paths.any? { |path| tp.path.start_with?(path) } || tp.path.start_with?(ruby_lib_path)
+class TracepointStack
+  def initialize
+    @gem_paths = Bundler.load.specs.map(&:full_gem_path)
+    @ruby_lib_path = RbConfig::CONFIG['rubylibdir']
+  end 
 
-  params = tp.binding.local_variables.map { |var|
-    [var, tp.binding.local_variable_get(var)]
-  }.to_h
-  
-  puts "called: #{tp.defined_class}##{tp.method_id} in #{tp.path}:#{tp.lineno} with params: #{params}"
+  def tracer
+    @trace ||=  TracePoint.new(:call) do |tp|
+      next if start_with_to_ignore_prefixes?(tp) || from_gempath_or_lib_path?(tp)
+
+      params = fetch_params(tp)
+      
+      puts "called: #{tp.defined_class}##{tp.method_id} in #{tp.path}:#{tp.lineno} with params: #{params}"
+    end
+  end
+
+  private
+  attr_reader :gem_paths, :ruby_lib_path
+
+  def fetch_params(tp)
+    tp.binding.local_variables.map { |var|
+      [var, tp.binding.local_variable_get(var)]
+    }.to_h
+  end
+
+  def start_with_to_ignore_prefixes?(tp)
+    tp.path.start_with?('<internal:') || tp.path == '(eval)'
+  end
+
+  def from_gempath_or_lib_path?(tp)
+    gem_paths.any? { |path| tp.path.start_with?(path) } || tp.path.start_with?(ruby_lib_path)
+  end
 end
 
-trace.enable
+tracer = TracepointStack.new.tracer
+tracer.enable
 
 at_exit do
   trace.disable
