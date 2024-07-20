b7fe1be16723a3ac0b33f014d29c3776ea9794e7
fabiosammy <fabiosammy@gmail.com>
2024-07-20 00:24:01 -0300

    Add a dummy test for the json log_format

--- spec/tracer_spec.rb
+++ spec/tracer_spec.rb
@@ -18,6 +18,9 @@ RSpec.describe RailsTracepointStack::Tracer do
         .and_return(false)
 
       allow(RailsTracepointStack::Logger).to receive(:log)
+
+      allow_any_instance_of(TracePoint).to receive(:path).and_return("/app/rails_tracepoint_stack/spec/tracer_spec.rb")
+      allow_any_instance_of(TracePoint).to receive(:lineno).and_return(6)
     end
 
     it 'calls logger with correct log' do
@@ -27,7 +30,20 @@ RSpec.describe RailsTracepointStack::Tracer do
 
       expect(RailsTracepointStack::Logger)
         .to have_received(:log)
-        .with(/called: Foo#dummy_method/)
+        .with("called: Foo#dummy_method in /app/rails_tracepoint_stack/spec/tracer_spec.rb:6 with params: {}")
+    end
+
+    # TODO: Extract this test to a proper place
+    it 'calls logger with correct log with json log format' do
+      allow_any_instance_of(RailsTracepointStack::Configuration).to receive(:log_format).and_return(:json)
+
+      tracer.tracer.enable do
+        Foo.new.dummy_method
+      end
+
+      expect(RailsTracepointStack::Logger)
+        .to have_received(:log)
+        .with("{\"class\":\"Foo\",\"method_id\":\"dummy_method\",\"path\":\"/app/rails_tracepoint_stack/spec/tracer_spec.rb\",\"line\":6,\"params\":{}}")
     end
   end
 
