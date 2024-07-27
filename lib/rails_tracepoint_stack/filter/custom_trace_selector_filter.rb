d70b6eaeb6983b077f42075e821fb56e3e164c12
carlosdanielpohlod <carlospohlod@gmail.com>
2024-07-27 16:12:52 -0300

    removed some prints

--- lib/rails_tracepoint_stack/filter/custom_trace_selector_filter.rb
+++ lib/rails_tracepoint_stack/filter/custom_trace_selector_filter.rb
@@ -2,9 +2,6 @@ module RailsTracepointStack
   module Filter
     class CustomTraceSelectorFilter
       def self.ignore_trace?(trace:)
-        p RailsTracepointStack.configuration.file_path_to_filter_patterns
-        p trace.file_path
-        p filter_match_a_custom_pattern_to_be_not_ignored?(trace)
         return false unless RailsTracepointStack.configuration.file_path_to_filter_patterns.any?
 
         !filter_match_a_custom_pattern_to_be_not_ignored?(trace)
